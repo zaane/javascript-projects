784b8fa1dcb76bba10be5207217c68ebef20323b
zane o <zaneolds@gmail.com>
2024-03-28 12:16:55 -0500

    create dummy dialog button and dialog

--- library/script.js
+++ library/script.js
@@ -57,3 +57,16 @@ function addRowToTable(book) {
 for (book of myLibrary) {
     addRowToTable(book);
 }
+
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener('click', () => {
+    dialog.show();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
